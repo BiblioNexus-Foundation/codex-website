5a20a3d779600c1cfe6a6c9fe5c71eeb92713260
ryderwishart <ryderwishart@gmail.com>
2024-07-18 16:49:42 -0600

    Build configuration changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,16 @@
-import sveltePreprocess from "svelte-preprocess";
 import adapter from '@sveltejs/adapter-auto';
+import { preprocessMeltUI } from '@melt-ui/pp';
+import sequence from 'svelte-sequential-preprocessor';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [
-    sveltePreprocess({
-      postcss: true,
-    }),
-  ],
+  preprocess: sequence([
+    vitePreprocess(),
+    preprocessMeltUI()
+  ]),
   kit: {
-    adapter: adapter(),
-    alias: {
-      $lib: './src/lib'
-    }
+    adapter: adapter()
   }
 };
 
